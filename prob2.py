b7ea7c8672ae9cedf245aaa5e73660380ee7ed6f
ASUS-VIVOBOOK-1\Srikaran <srikaran2230@gmail.com>
2022-10-08 16:10:06 +0530

    problem

---
+++ prob2.py
@@ -0,0 +1,15 @@
+marks=int(input("Enter your marks:"))
+
+if marks>90 and marks<=100:
+    print("Your grade is A")
+elif marks>80 and marks<=90:
+    print("Your grade is B")
+elif marks>70 and marks<=80:
+    print("Your grade is C")
+elif marks>70 and marks<=60:
+    print("Your grade is D")
+else:
+    print("Enter a valid number")
+
+
+
