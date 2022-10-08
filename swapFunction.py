c5b283f799d0e7785244d099b22af1ea23565f2e
ASUS-VIVOBOOK-1\Srikaran <srikaran2230@gmail.com>
2022-10-08 15:39:54 +0530

    swapFunction

---
+++ swapFunction.py
@@ -0,0 +1,10 @@
+#Take inputs from user for two numbers (x and y)
+x = 5
+y = 4
+print("value of x is :" ,x)
+print("value of y is :" ,y)
+temp = x 
+print("value of temp is :" , temp)
+x = y
+print("value of x is :" ,x)
+y=temp
