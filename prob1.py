88ee9ac13b600887e871971fd7b89d25af6ad036
ASUS-VIVOBOOK-1\Srikaran <srikaran2230@gmail.com>
2022-10-08 15:06:25 +0530

    add

---
+++ prob1.py
@@ -0,0 +1,13 @@
+
+    #A Company decided to give bonus of 1000Rs to 
+    #employee if his/her service is more than 5 years 
+    #Ask user their salary and year of service and print
+    #the net bonus amount
+
+a = float(input("salary"))
+b = float(input("years of experience"))
+
+if b>5:
+        print("total amount",1000)
+else:
+        print("total amount",0)
