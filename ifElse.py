659063f226ee1e668b9be414337f9b8d2450d336
ASUS-VIVOBOOK-1\Srikaran <srikaran2230@gmail.com>
2022-10-08 15:05:43 +0530

    adding

---
+++ ifElse.py
@@ -0,0 +1,24 @@
+#take x and y two variables and assign them value
+#if x is less than y,print YAYY
+#else print NAVY
+
+x = float(input("Enter first no:"))
+y =float(input("Enter second no:"))
+
+if x <y:
+    print("YAYY")
+else:
+    print("NAVY")
+
+length = float(input("enter length:"))
+breadth = float(input("enter breadth:"))
+
+if length==breadth:
+    print("It's a Square")
+
+else:
+    print("its not a square")
+
+
+
+
