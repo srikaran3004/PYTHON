3a3e56d2145c425a4100f2d22d5c9a1b6878efc4
ASUS-VIVOBOOK-1\Srikaran <srikaran2230@gmail.com>
2022-10-08 15:03:06 +0530

    adding

---
+++ area.py
@@ -0,0 +1,14 @@
+#Area of triangle using herons formula
+
+a = float(input("enter first number: "))
+b = float(input("enter second number:"))
+c = float(input("enter third number:"))
+
+s = (a+b+c)/2
+
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+
+print("area")
+
+
+
