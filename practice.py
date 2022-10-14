263bcc2e298a36fb3f829a8d78680c3b72dc76c5
ASUS-VIVOBOOK-1\Srikaran <srikaran2230@gmail.com>
2022-10-14 18:40:17 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,12 @@
+print("DREAM TEAM")
+match1 =float(input("Enter your batting avarage"))
+match2 =float(input("Enter your batting avarage"))
+match3 =float(input("Enter your batting avarage"))
+match4 =float(input("Enter your batting avarage"))
+match5 =float(input("Enter your batting avarage"))
+sum =match1 + match2 + match3+match4+match5
+avg = sum/5
+print("Average",avg)
+
+
+
