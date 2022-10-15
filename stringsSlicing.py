492dcb34ff5a2df85bb2f03c60e7ac8c3f9ee0f7
ASUS-VIVOBOOK-1\Srikaran <srikaran2230@gmail.com>
2022-10-15 16:11:49 +0530

    add

---
+++ stringsSlicing.py
@@ -0,0 +1,32 @@
+a = "Hello world"
+print(a[-4:-2]) 
+print(a[2:])
+print(a[:4])
+print(a.upper())
+print(a.lower())
+
+#strip
+a = "      Hello World"
+print (a)
+print(a.strip())
+
+#replace
+a = "    Hello World"
+print(a)
+print(a.replace("H","Y"))
+print(a.replace("Hello" , "Hey"))
+
+#captalize
+a = "hello world"
+print(a.capitalize())
+
+
+#Refer python string methods
+#geeks for geeks
+#w3 schools  
+
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
