b3e98e005b435c5a3631e84c7288ee7bbff380bc
ASUS-VIVOBOOK-1\Srikaran <srikaran2230@gmail.com>
2022-10-15 16:49:33 +0530

    add

---
+++ problem_string.py
@@ -0,0 +1,11 @@
+#take a string of length 5 [index 0-4]
+#print first char
+#print middle char
+#print last char
+
+
+x = input("Enter a string")
+y = len(x)//2
+print(x[0])
+print(x[-1])
+print(x[y])
