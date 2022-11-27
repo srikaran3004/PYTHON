27b9098aff3fe5d84a3edf48c5028a68934bc253
Srikaran <srikaran2230@gmail.com>
2022-11-27 11:09:10 +0530

    Sets

---
+++ sets.py
@@ -0,0 +1,12 @@
+#Store multiple values in single variable 
+#Set is unordered
+# mySet={'car','bike','train'}
+# print(mySet)
+# Unchangable
+# Duplicates are not allowed (double times are not allowed)
+
+
+
+
+
+
