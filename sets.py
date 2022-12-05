6da2dc5db0b3088dec4f3d8eb3cc8475b3b9b5d1
Srikaran Devarakonda <srikaran2230@gmail.com>
2022-12-05 14:22:43 +0530

    Python

--- sets.py
+++ sets.py
@@ -5,6 +5,22 @@
 # Unchangable
 # Duplicates are not allowed (double times are not allowed)
 
+# mySet={"cars","bike","boat","train"}
+# mySet.add("cycle")
+# mySet.clear
+# mySet.discard
+# mySet1={1,2,3,4}
+# mySet2=mySet.union(mySet1)
+# print(mySet2)
+# mySet2={4,5,6}
+# mySet3=mySet1.union(mySet2)
+# print(mySet3)
+# myset4=mySet1.symmetric_difference(mySet2)
+# Symmetric Difference prints the elememts that are not in common for the given two sets.
+# print(myset4)
+
+
+
 
 
 
